785b58c48c97a02c0fb621eb991e7c306ee1e708
ivanw98 <Ivan98@live.co.uk>
2024-11-17 18:55:03 +0000

    function evals

--- object/environment.go
+++ object/environment.go
@@ -1,18 +1,32 @@
 package object
 
+// Environment represents a storage for objects, maintaining a mapping between variable names and their corresponding objects.
 type Environment struct {
 	store map[string]Object
+	outer *Environment
+}
+
+// NewEnclosedEnvironment creates a new Environment containing a reference to an outer Environment for nested scopes.
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
 }
 
 // NewEnvironment creates and returns a new Environment with an empty store.
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
-	return &Environment{store: s}
+	return &Environment{
+		store: s,
+	}
 }
 
 // Get retrieves an Object from the Environment by its name, returning the Object and a boolean indicating its presence.
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
--- object/object.go
+++ object/object.go
@@ -1,7 +1,10 @@
 package object
 
 import (
+	"bytes"
 	"fmt"
+	"monkey/ast"
+	"strings"
 )
 
 // ObjectType defines the type for various objects in the system.
@@ -13,6 +16,7 @@ const (
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
 )
 
 type Object interface {
@@ -33,6 +37,13 @@ type Boolean struct {
 // ReturnValue represents the return value object in the system, wrapping an Object that contains the actual returned value.
 type ReturnValue struct {
 	Value Object
+}
+
+// Function represents a function as an object.
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
 }
 
 // Error represents an error object in the system with a message.
@@ -92,4 +103,28 @@ func (n *Null) Inspect() string {
 // Type returns the object type.
 func (n *Null) Type() ObjectType {
 	return NULL_OBJ
+}
+
+// Inspect returns a string representation of a Function object.
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+	var params []string
+
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
+
+// Type returns the object type.
+func (f *Function) Type() ObjectType {
+	return FUNCTION_OBJ
 }
