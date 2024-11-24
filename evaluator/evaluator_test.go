b25ddfec922e5b8c09e72b82bc7f23bf06fb50a7
ivanw98 <Ivan98@live.co.uk>
2024-11-24 22:52:23 +0000

    Concatenating strings

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -289,6 +289,10 @@ if (10 > 1) {
 			"foobar",
 			"identifier not found: foobar",
 		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
 	}
 
 	for _, tt := range tests {
@@ -386,6 +390,20 @@ func TestStringLiteral(t *testing.T) {
 
 	if str.Value != "Hello world" {
 		t.Errorf("String has wrong value. want=%q, got=%q", "Hello world", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not string. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has the wrong value. got=%q", str.Value)
 	}
 }
 
