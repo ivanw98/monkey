cf0e8531bc6d1ecd56e4be9c49a02ac2e25133fe
Ivan Wills <ivan.wills@paddle.com>
2026-04-18 17:15:20 +0100

    Test gh actions again

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -223,6 +223,10 @@ func (c *Compiler) Compile(node ast.Node) error {
 
 		c.loadSymbol(symbol)
 
+	case *ast.StringLiteral:
+		str := &object.String{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(str))
+
 	case *ast.ArrayLiteral:
 		for _, element := range node.Elements {
 			err := c.Compile(element)
